523d84fd6cc801e17cef0e7cef2e36a6ef135961
tgrabar <ted.grabar@gmail.com>
2023-08-25 22:39:33 -0700

    Add function to get player input

--- rps.js
+++ rps.js
@@ -1,11 +1,27 @@
 function getComputerChoice() {
     // Get random number between 0-2 and return the result as a string
     switch (Math.floor(Math.random() * 3)) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
-        default: return "Rock";
+        case 0: return 'Rock';
+        case 1: return 'Paper';
+        case 2: return 'Scissors';
+        default: return 'Rock';
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerChoice = prompt('Choose Rock, Paper, or Scissors', '');
+
+    // Check for one of the three valid choices, case-insensitive
+    switch (playerChoice.toUpperCase()) {
+        case 'ROCK': return 'Rock';
+        case 'PAPER': return 'Paper';
+        case 'SCISSORS': return 'Scissors';
+        default: return null;
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
