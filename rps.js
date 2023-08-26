1c05d1f117930f491c6e709f6534d314f52096d0
tgrabar <ted.grabar@gmail.com>
2023-08-25 23:10:13 -0700

    Add function to play one round of RPS
    Todo: need to check for invalid player input

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    // Get random number between 0-2 and return the result as a string
+    // get random number between 0-2 and return the result as a string
     switch (Math.floor(Math.random() * 3)) {
         case 0: return 'Rock';
         case 1: return 'Paper';
@@ -11,7 +11,7 @@ function getComputerChoice() {
 function getPlayerChoice() {
     let playerChoice = prompt('Choose Rock, Paper, or Scissors', '');
 
-    // Check for one of the three valid choices, case-insensitive
+    // check for one of the three valid choices, case-insensitive
     switch (playerChoice.toUpperCase()) {
         case 'ROCK': return 'Rock';
         case 'PAPER': return 'Paper';
@@ -20,8 +20,31 @@ function getPlayerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    // check for ties first
+    if (playerSelection === computerSelection)
+        result = 'It\'s a tie!';
+    // check loss conditions
+    else if (
+        (playerSelection === 'Rock' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Rock')
+    )
+        result = 'You lose!';
+    else
+        result = 'You win!';
+    
+    // display what players chose
+    result += ` (You chose: ${playerSelection}, Opponent chose: ${computerSelection}\)`;
+
+    return result;
+}
+
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
 console.log(playerSelection);
 console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
