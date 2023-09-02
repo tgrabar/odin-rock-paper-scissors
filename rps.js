60b4ad66e5d35bffb227e06b1ec292b4f0c3e090
tgrabar <ted.grabar@gmail.com>
2023-09-02 00:33:08 -0700

    Replace player choice prompt with buttons

--- rps.js
+++ rps.js
@@ -3,7 +3,15 @@ let gamesWon = 0;
 let gamesLost = 0;
 let gamesTied = 0;
 
-game();
+const rpsBtn = document.querySelectorAll(".rpsbtn");
+
+rpsBtn.forEach((btn) =>
+        btn.addEventListener("click", () => {
+            playRound(btn.dataset.choice, getComputerChoice())
+            }
+));
+
+// game();
 
 function getComputerChoice() {
     // get random number between 0-2 and return the result as a string
@@ -12,22 +20,6 @@ function getComputerChoice() {
         case 1: return 'Paper';
         case 2: return 'Scissors';
         default: return 'Rock';
-    }
-}
-
-function getPlayerChoice() {
-    let playerChoice = prompt('Choose Rock, Paper, or Scissors', '');
-
-    // check for one of the three valid choices, case-insensitive
-    switch (playerChoice.toUpperCase()) {
-        case 'ROCK': return 'Rock';
-        case 'PAPER': return 'Paper';
-        case 'SCISSORS': return 'Scissors';
-        // return undefined for invalid selection, to be checked in game() function
-        default: {
-            alert('Invalid selection. Please enter rock, paper, or scissors.');
-            return;
-        }
     }
 }
 
@@ -56,28 +48,6 @@ function playRound(playerSelection, computerSelection) {
     
     // display what players chose
     result += ` (You chose: ${playerSelection}, Opponent chose: ${computerSelection}\)`;
-
+    console.log(result);
     return result;
-}
-
-function game() {
-    // play 5 rounds; ties count as a round
-    for(let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
-        let playerSelection;
-        // get player input, prompt again until valid
-        while (playerSelection === undefined) {
-            playerSelection =  getPlayerChoice();
-        }
-
-        console.log(playRound(playerSelection, getComputerChoice()));
-    }
-    // display final results
-    if(gamesWon === gamesLost)
-        console.log('Final result: It\'s a tie!');
-    else if (gamesWon < gamesLost)
-        console.log('Final result: You lose!');
-    else
-        console.log('You win!');
-
-    console.log('Player: ' + gamesWon + ', Opponent: ' + gamesLost + ', Ties: '+ gamesTied);
 }
